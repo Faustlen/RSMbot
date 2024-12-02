96c502f779e242639c92c34da0172b04edb9cfbd
Murat Khachemizov <m.hachemizov@mk.dunice.net>
2024-12-02 15:00:45 +0300

    git init

---
+++ src/main/java/net/dunice/mk/rsmtelegrambot/RsmTelegramBotApplication.java
@@ -0,0 +1,13 @@
+package net.dunice.mk.rsmtelegrambot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RsmTelegramBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RsmTelegramBotApplication.class, args);
+    }
+
+}
