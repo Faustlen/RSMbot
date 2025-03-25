9243730eefd00900e8bda28cb957d6347b7e27e3
Cheuchev Timur <AndryRaian.timurche@yandex.ru>
2025-03-25 10:49:38 +0300

    Create entity and migration fot table Stocks

---
+++ src/main/java/net/dunice/mk/rsmtelegrambot/entity/Stock.java
@@ -0,0 +1,43 @@
+package net.dunice.mk.rsmtelegrambot.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "stocks")
+public class Stock {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer stocksId;
+
+    @ManyToOne
+    @JoinColumn(name = "partner_tg_id", nullable = false)
+    private Partner partnerTgId;
+
+    @Column(nullable = false)
+    private byte[] image;
+
+    @Column(nullable = false)
+    private String head;
+
+    @Column(nullable = false)
+    private LocalDate periodStocksS;
+
+    @Column(nullable = false)
+    private LocalDate periodStocksE;
+
+    @Column(nullable = false)
+    private String description;
+}
